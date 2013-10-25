9f22ff23cbc542915a3625fe9c57f428f58ffdc7
yusuf <yusuf.el@gmail.com>
2013-10-25 01:22:41 -0700

    Js expanded

--- WikiAPI.js
+++ WikiAPI.js
@@ -40,40 +40,104 @@ $(function () {
 
 function parse_data(data) {
 
+    var PageData = new Object();
+    PageData.id = "";
+    PageData.title = "";
+    PageData.description = "";
+    PageData.picture = "";
+    PageData.date = ""
+    PageData.year_start = "";
+    PageData.year_end = "";
+    PageData.population = "";
+    PageData.population_value = "";
+    PageData.total_deaths = "";
+    PageData.total = "";
 
-
+    //Break down returned object
     var entry = data.query.pages
-    var en
 
+    //Get PageID
+    var en
     for (var name in entry) {
         if (entry.hasOwnProperty(name)) {
             en = entry[name];
+            PageData.id = en.pageid
         }
     }
     var raw = en.revisions[0]
 
+    //Get Title
+    PageData.title = en.title;
+
     //Get Type
     var page_type
 
+    //Attach HTML or Text from API response to DOM, which 
+    //allows you to serach with jquery as opposed to parsing HTML as a string.
     var el = $('<div></div>');
     var jHTML = $('<div/>').append(raw['*'])
 
-    var description = jHTML.find('p').first().html();
+    //Get Descripition
+    PageData.description = jHTML.find('p').first().html();
     var tmp = document.createElement("DIV");
-    tmp.innerHTML = description;
+    tmp.innerHTML = PageData.description;
+    PageData.description = tmp.textContent || tmp.innerText || "";
 
-    var picture = jHTML.find('.vcard img:first').attr('src');
+    //Get Image
+    PageData.picture = jHTML.find('.vcard img:first').attr('src');
+    if (!PageData.picture) {
+        PageData.picture = jHTML.find('.vevent img:first').attr('src')
+    }
 
-    var PageData = new Object();
-    PageData.title = en.title;
-    PageData.description = tmp.textContent || tmp.innerText || ""; ;
-    PageData.picture = picture;
-    PageData.year_start = 1969;
-    PageData.year_end = 1969;
+    //Get Date
+    PageData.date = jHTML.find('.vevent th:contains(Date)').next("td").html();
+    var tmp2 = document.createElement("DIV");
+    tmp2.innerHTML = PageData.date;
+    PageData.date = tmp2.textContent || tmp2.innerText || "";
+
+
+    //Get Population
+    PageData.population = jHTML.find('th:contains(Population)').parent().next('tr').find('th:contains(City)').next('td').html();
+
+    if (!PageData.population) {
+        PageData.population = jHTML.find('th:contains(Population)').parent().next('tr').find('td:contains(census)').next('td').html()//.next('td').html();
+    };
+    if (!PageData.population) {
+        PageData.population = jHTML.find('th:contains(Population)').parent().next('tr').next('tr').find('td:contains(census)').next('td').html()//.next('td').html();
+    };
+    if (!PageData.population) {
+        PageData.population = jHTML.find('th:contains(Population)').parent().next('tr').find('td:contains(estimate)').next('td').html()//.next('td').html();
+    }
+    if (!PageData.population) {
+        PageData.population = jHTML.find('th:contains(Population)').parent().next('tr').next('tr').find('td:contains(estimate)').next('td').html()//.next('td').html();
+    }
+
+    var tmp3 = document.createElement("DIV");
+    tmp3.innerHTML = PageData.population;
+    PageData.population = tmp3.textContent || tmp3.innerText || "";
+
+    if (PageData.population.indexOf('(') !== -1) {
+        PageData.population = PageData.population.substring(0, PageData.population.indexOf('('));
+    }
+    if (PageData.population.indexOf('[') !== -1) {
+        PageData.population = PageData.population.substring(0, PageData.population.indexOf('['));
+    }
+
+    PageData.population = PageData.population.replace(' ', '').replace(',', '').replace(',', '').replace(',', '')
+    PageData.population = Math.round(((Number(PageData.population) / 7046000000) * 1) * 100)
+
+
+
+
+
+
+
 
     //Change front end with new values
     $('.page-header').html('<h1>' + PageData.title + '</h1> <br/> ' + PageData.description + '')
-    $(".jumbotron").attr("style", "background: url(" + picture + "); -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;");
-    $('#profile_image').attr("src", picture); 
-
+    $(".jumbotron").attr("style", "background: url(" + PageData.picture + "); -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;");
+    $('#profile_image').attr("src", PageData.picture);
+    $('#Profile').attr("class", "panel-collapse");
+    $('#page_date').html(PageData.date);
+    $('#page_population_progressbar').css('width', PageData.population)
 };
